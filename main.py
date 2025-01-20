7637107d380c1c29aae08809d9c11a102b688028
Ivan-49 <remnevvanya@yandex.ru>
2025-01-21 00:27:40 +0300

    all

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
